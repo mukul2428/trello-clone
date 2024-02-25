156cf0407d3727fff447d02b32e00236a5f255e6
Mukul Raghav <66879197+mukul2428@users.noreply.github.com>
2024-02-25 23:01:21 +0530

    added few classes

--- src/components/header-section/index.tsx
+++ src/components/header-section/index.tsx
@@ -3,34 +3,39 @@ import "./header.css";
 
 const HeaderSection = () => {
   return (
-    <header className="flex header-container">
-      <a className="flex padding-1 header-logo" href="/">
-        <img src={IMAGES.logo.image} alt={IMAGES.logo.alt} />
-      </a>
-      <div className="flex">
-        {NAVITEMS.map((item) => {
-          return (
-            <button className="header-btn" key={item.id}>
-              <span>{item.itemName}</span>
-              {item.itemName !== "Pricing" && (
-                <img
-                  src={IMAGES.down_arrow.image}
-                  alt={IMAGES.down_arrow.alt}
-                />
-              )}
-            </button>
-          );
-        })}
-      </div>
-      <div className="flex towards-right">
-        <a className="anchor-none login vertical-center" href="">
-          Login
-        </a>
-        <a className="anchor-none get-trello vertical-center" href="">
-          Get Trello for free
-        </a>
-      </div>
-    </header>
+    <>
+      <header className="header-top">
+        <div className="flex header-container">
+          <a className="flex padding-1 header-logo" href="/">
+            <img src={IMAGES.logo.image} alt={IMAGES.logo.alt} />
+          </a>
+          <div className="flex">
+            {NAVITEMS.map((item) => {
+              return (
+                <button className="header-btn" key={item.id}>
+                  <span>{item.itemName}</span>
+                  {item.itemName !== "Pricing" && (
+                    <img
+                      src={IMAGES.down_arrow.image}
+                      alt={IMAGES.down_arrow.alt}
+                    />
+                  )}
+                </button>
+              );
+            })}
+          </div>
+          <div className="flex towards-right">
+            <a className="anchor-none login vertical-center" href="">
+              Login
+            </a>
+            <a className="anchor-none get-trello vertical-center" href="">
+              Get Trello for free
+            </a>
+          </div>
+        </div>
+      </header>
+      <div style={{ height: "110vh" }}></div>
+    </>
   );
 };
 
